4278fa3d0dbeb5b3687b77354dd0f17954baffd4
Mariachi in a Jar <mariachiinajar@gmail.com>
2023-02-07 23:17:50 +0900

    updated scripts

---
+++ automate1.sh
@@ -0,0 +1,2 @@
+bash install_git
+git clone https://github.com/mariachiinajar/ecs-project1-ec2.git
--- automate2.sh
+++ automate2.sh
@@ -1,5 +1,3 @@
-bash install_git
-git clone https://github.com/mariachiinajar/ecs-project1-ec2.git
 bash /home/ubuntu/ecs-project1-ec2/install_docker.sh
 bash /home/ubuntu/ecs-project1-ec2/getec2ip.sh
 bash /home/ubuntu/run_project.sh
