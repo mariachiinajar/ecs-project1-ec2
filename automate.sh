434fb7df9e6930591f77beee31ddbde30502f664
Mariachi in a Jar <mariachiinajar@gmail.com>
2023-02-07 23:14:19 +0900

    updated scripts

--- automate.sh
+++ automate.sh
@@ -1,3 +1,5 @@
-bash install_docker.sh
-bash getec2ip.sh
-bash run_project.sh
+bash install_git
+git clone https://github.com/mariachiinajar/ecs-project1-ec2.git
+bash /home/ubuntu/ecs-project1-ec2/install_docker.sh
+bash /home/ubuntu/ecs-project1-ec2/getec2ip.sh
+bash /home/ubuntu/run_project.sh
--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,6 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -11,6 +11,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo docker run hello-world
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "Docker successfully installed."
 docker --version
